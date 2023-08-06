3cd05083ec46514ec357feb525bbbda1779d38aa
k-18123 <nguyenkhang181203@gmail.com>
2023-08-06 16:24:28 +0700

    part 11.1

---
+++ backend/src/data.ts
@@ -0,0 +1,72 @@
+
+export const sample_books: any[]=[
+    {
+        id: '0',
+        name: "Đi vòng quanh thế giới vẫn quanh 1 người",
+        price: 10,
+        favorite:true,
+        categories: ['Văn học'],
+        decription: "sách Đi vòng quanh thế giới vẫn quanh 1 người",
+        imageUrl:"assets/divongquanhtgvanquanh1nguoi.jpg"
+    },
+    {
+        id: '1',
+        name: "Có 2 con mèo ngồi bên cửa sổ",
+        price: 6,
+        favorite:true,
+        categories: ['Văn học'],
+        decription: "sách Có 2 con mèo ngồi bên cửa sổ",
+        imageUrl:'assets/cohaiconmeongoibencuaso.jpg'
+    },
+    {
+        id: '2',
+        name: "Lạc giữa tần số không người nghe",
+        price: 12,
+        favorite:true,
+        categories: ['Tâm lí'],
+        decription: "sách Lạc giữa tần số không người nghe",
+        imageUrl:'assets/lacgiuatansokhongnguoinghe.jpg'
+    },
+    {
+        id: '3',
+        name: "Mẹ làm gì có ước mơ",
+        price: 11,
+        favorite:true,
+        categories: ['Văn học'],
+        decription: "sách Mẹ làm gì có ước mơ",
+        imageUrl:'assets/melamgicouocmo.jpg'
+    },
+    {
+        id: '4',
+        name: "Ngày đó mèo đến bên tôi",
+        price: 5,
+        favorite:false,
+        categories: ['Văn học'],
+        decription: "sách Ngày đó mèo đến bên tôi",
+        imageUrl:'assets/ngaydomeodenbentoi.jpg'
+    },
+    {
+        id: '5',
+        name: "Tấm vé tuổi thơ",
+        price: 7,
+        favorite:true,
+        categories: ['Văn học'],
+        decription: "sách Tấm vé tuổi thơ",
+        imageUrl:'assets/tamvetuoitho.jpg'
+    },
+    {
+        id: '6',
+        name: "Tôi thấy hoa vàng trên đồng cỏ xanh",
+        price: 8,
+        favorite:true,
+        categories: ['Văn học'],
+        decription: "Sách tôi thấy hoa vàng trên đồng cỏ xanh",
+        imageUrl:'assets/toithayhoavangtrendongcoxanh.jpg'
+    },
+]
+
+export const sample_categories: any[] = [
+    {name: 'Văn học', count: 6},
+    {name: 'Tâm lí',count: 1},
+    {name: 'Khoa học', count: 0}
+]
---
+++ backend/src/server.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors"
+import { sample_books, sample_categories } from "./data";
+import { Category } from './../../frontend/src/app/shared/models/Category';
+
+const app= express();
+app.use(cors({
+    credentials:true,
+    origin:["http://localhost:4200"]
+}));
+
+app.get("/api/books", (req, res) => {
+    res.send(sample_books);
+})
+
+app.get("/api/books/search/:searchTerm", (req, res) => {
+    const searchTerm  = req.params.searchTerm;
+    const books = sample_books.filter(book => book.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    res.send(books)
+  })
+
+app.get("/api/books/categories", (req, res) => {
+    res.send(sample_categories);
+})
+
+app.get("/api/books/category/:categoryName", (req, res) => {
+    const categoryName = req.params.categoryName;
+    const books = sample_books
+    .filter(book => book.categories?.includes(categoryName));
+    res.send(books);
+})
+
+app.get("/api/books/:bookId", (req, res) => {
+    const bookId = req.params.bookId;
+    const book = sample_books.find(book => book.id == bookId);
+    res.send(book);
+})
+
+const port = 5000;
+app.listen(port, () => {
+    console.log("Website served on http://localhost:" + port);
+})
